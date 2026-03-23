dcab35f52683e5ab38bb7b816c66a1b5a5181c84
brunoassis07 <brunopansani2007@gmail.com>
2026-03-23 12:41:54 -0300

    otimizando e finalizando o código

--- index.js
+++ index.js
@@ -1,45 +1,13 @@
-let xp = 7500;
-let nivel;
-let nome;
+const xp = 7500;
+let nivel, nome;
 
-if (xp < 1000) {
-    nivel = "Ferro";
-    nome = "Ragnar";
-}
-
-if (xp >= 1000 && xp < 2000) {
-    nivel = "Bronze";
-    nome = "Ryan";
-}
-
-if (xp >= 2000 && xp < 5000) {
-    nivel = "Prata";
-    nome = "Lara";
-}
-
-if (xp >= 5000 && xp < 7000) {
-    nivel = "Ouro";
-    nome = "Goku";
-}
-
-if (xp >= 7000 && xp < 8000){
-    nivel = "Platina";
-    nome = "Saitama";
-}
-
-if (xp >= 8000 && xp < 9000) {
-    nivel = "Ascendente";
-    nome = "Dante";
-}
-
-if (xp >= 9000 && xp < 10000) {
-    nivel = "Imortal";
-    nome = "Kratos";
-}
-
-if (xp >= 10000) {
-    nivel = "Radiante";
-    nome = "Thor";
-}
+if (xp >= 10000)       { nivel = "Radiante";   nome = "Thor";    }
+else if (xp >= 9000)   { nivel = "Imortal";    nome = "Kratos";  }
+else if (xp >= 8000)   { nivel = "Ascendente"; nome = "Dante";   }
+else if (xp >= 7000)   { nivel = "Platina";    nome = "Saitama"; }
+else if (xp >= 5000)   { nivel = "Ouro";       nome = "Goku";    }
+else if (xp >= 2000)   { nivel = "Prata";      nome = "Lara";    }
+else if (xp >= 1000)   { nivel = "Bronze";     nome = "Ryan";    }
+else                   { nivel = "Ferro";       nome = "Ragnar";  }
 
 console.log(`O Herói de nome ${nome} está no nível ${nivel}.`);
